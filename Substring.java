0c4fd21313ac0ba5ed9257edf9f26f11270bb55e
MD Raja Ansari <mdrajaansari9555@gmail.com>
2026-04-06 23:06:08 +0530

    added new file

---
+++ Substring.java
@@ -0,0 +1,7 @@
+public class Substring {
+    public static void main(String[] args) {
+        String s1 ="hello world";
+        String s2 ="hello";
+        System.out.println(s1.contains(s2) ? "found" :"not found");
+    }
+}
