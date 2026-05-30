a69524587602b323f38f83ac4773e4c86ad8b1f6
MD Raja Ansari <mdrajaansari9555@gmail.com>
2026-05-31 05:13:50 +0530

    added new file

---
+++ SingleNumber.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber {
+    public static int singleNumber(int[] nums){
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        //count frequency
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num , 0)+1);
+        }
+
+        for(Map.Entry<Integer ,Integer> entry :map.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+         return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 3, 1, 2, 4};
+        int result = singleNumber(nums);
+        System.out.println(result);
+    }
+
+}
