cb3d17e3fdd2d578dc6bd1f7e2f84c2827f23842
MD Raja Ansari <mdrajaansari9555@gmail.com>
2026-06-08 23:10:55 +0530

    fix issues

--- Swap.java
+++ Swap.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Swap {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter your First Number :");
+       int a = sc.nextInt();
+       System.out.println("Enter you second Number:");
+       int b = sc.nextInt();
         System.out.println("before swapping"+ "a = " + a +"b =" +b);
        a=a^b;
        b=a^b;
