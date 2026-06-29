36867bb9e9e9c2739ee8c4364a58a3b93341590d
MD Raja Ansari <mdrajaansari9555@gmail.com>
2026-06-29 23:45:27 +0530

    added new file

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -2,18 +2,18 @@ import java.util.Arrays;
 
 public class RemoveDuplicate {
     public static int removeDuplicate(int[] nums){
-        Arrays.sort(nums);
-        int j =0;
-        for(int i =1; i<nums.length; i++){
-            if(nums[i] !=  nums[j]){
-                j++;
-                nums[j] = nums[i];
-            }
+      Arrays.sort(nums);
+      int j =0;
+      for(int i =0; i<nums.length;i++){    //!find duplicate for(i=1 to n) if(nums[i] != nums[i-1]) {Sop(nums(i))}
+        if(nums[j] != nums[i]){
+          j++;
+          nums[j] = nums[i];
         }
-        return j+1;
+      }
+      return j+1;
     }
    public static void main(String[] args) {
-    int[] nums = {1,2,3,5,1,3};
+    int[] nums = {1,2,3,5,1,3,3,2,1};
     int result = removeDuplicate(nums);
     for(int i=0; i<result; i++){
           System.out.println(nums[i]);
